05c9e1c97fb7b59615650478ef1e1d1ed2d018a7
XiaoChen515144 <3788872571@qq.com>
2026-09-13 11:27:37 +0800

    fix: use stable device tree path in device.mk

--- device.mk
+++ device.mk
@@ -1,4 +1,4 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := device/oneplus/OP5D2BL1
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
