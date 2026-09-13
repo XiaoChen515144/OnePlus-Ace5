960f49dafd4636633f5a6c9b4085cf0132fed9cf
XiaoChen515144 <3788872571@qq.com>
2026-09-13 13:52:39 +0800

    Simplify PRODUCT_COPY_FILES in device.mk
    Removed unnecessary module files from PRODUCT_COPY_FILES.

--- device.mk
+++ device.mk
@@ -1,10 +1,4 @@
 LOCAL_PATH := device/oneplus/OP5D2BL1
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
-    $(LOCAL_PATH)/prebuilt/modules/modules.alias:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.alias \
-    $(LOCAL_PATH)/prebuilt/modules/modules.blocklist:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.blocklist \
-    $(LOCAL_PATH)/prebuilt/modules/modules.dep:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.dep \
-    $(LOCAL_PATH)/prebuilt/modules/modules.load.recovery:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.load \
-    $(LOCAL_PATH)/prebuilt/modules/modules.softdep:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/modules.softdep
-
+    $(LOCAL_PATH)/recovery/root/first_stage_ramdisk/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom
